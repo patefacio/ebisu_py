099fb48578fdf5c954b71065a89fc6605d2fabb4
Daniel Davidson <dbdavidson@yahoo.com>
2016-04-27 11:07:24 -0500

    added test_class.dart

---
+++ test/test_class.dart
@@ -0,0 +1,27 @@
+library ebisu_py.test_class;
+
+import '../lib/ebisu_py.dart';
+import 'package:logging/logging.dart';
+import 'package:test/test.dart';
+
+// custom <additional imports>
+// end <additional imports>
+
+final _logger = new Logger('test_class');
+
+// custom <library test_class>
+// end <library test_class>
+
+main([List<String> args]) {
+  Logger.root.onRecord.listen(
+      (LogRecord r) => print("${r.loggerName} [${r.level}]:\t${r.message}"));
+  Logger.root.level = Level.OFF;
+// custom <main>
+
+  test('class basics', () {
+    final cls = new Class('x_y_z');
+    print(cls.definition);
+  });
+
+// end <main>
+}
